c94fc37e1a7dd14d3fd422f46eec84895200da00
ManojGnanapalam <manojgsret@gmail.com>
2024-07-17 19:23:38 +0530

    router boilerplate done

--- main.go
+++ main.go
@@ -1,15 +1,17 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
 	"log"
+	"net/http"
 	"os"
 
+	"github.com/go-chi/chi"
+	"github.com/go-chi/cors"
 	"github.com/joho/godotenv"
 )
 
 func main() {
-	fmt.Println("Hello world")
 	godotenv.Load(".env")
 
 	portNum := os.Getenv("PORT")
@@ -17,5 +19,65 @@ func main() {
 	if portNum == "" {
 		log.Panic("Port is not found in the environment")
 	}
-	fmt.Println("Port:", portNum)
+
+	router := chi.NewRouter()
+	router.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"https://*", "http://*"},
+		AllowedMethods:   []string{http.MethodGet, http.MethodPost, http.MethodDelete, http.MethodOptions},
+		AllowedHeaders:   []string{"*"},
+		ExposedHeaders:   []string{"Link"},
+		MaxAge:           300,
+		AllowCredentials: false,
+	}))
+
+	v1Router := chi.NewRouter()
+	v1Router.Get("/ready", handlerReadiness)
+	v1Router.Get("/err", handlerErr)
+
+	router.Mount("/v1", v1Router)
+
+	srv := &http.Server{
+		Handler: router,
+		Addr:    ":" + portNum,
+	}
+	log.Printf("server listen on Port %v", portNum)
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+}
+
+func handlerReadiness(w http.ResponseWriter, r *http.Request) {
+	respondJSON(w, 200, struct{}{})
+}
+
+func handlerErr(w http.ResponseWriter, r *http.Request) {
+	responseWithError(w, 400, "something wrong")
+}
+
+func responseWithError(w http.ResponseWriter, code int, msg string) {
+	if code > 499 {
+		log.Println("Responding with 5xx error: ", msg)
+	}
+
+	type errResponse struct {
+		Error string `josn:"error"`
+	}
+
+	respondJSON(w, code, errResponse{
+		Error: msg,
+	})
+}
+func respondJSON(w http.ResponseWriter, code int, payload interface{}) {
+	dat, err := json.Marshal(payload)
+
+	if err != nil {
+		log.Printf("Failed to marshal JSON response : %v", payload)
+		w.WriteHeader(500)
+		return
+	}
+	w.Header().Add("Content-Type", "application/json")
+	w.WriteHeader(code)
+	w.Write(dat)
 }
